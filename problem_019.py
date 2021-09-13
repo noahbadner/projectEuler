57a9b15934addaba8faf974d1a6307a3071c733c
noahbadner <noahbadner@gmail.com>
2021-09-12 21:56:40 -0400

    Add solutions for problems 19 and 21

--- problem_019.py
+++ problem_019.py
@@ -20,13 +20,29 @@
 MONTH_NUM_OF_DAYS_DICT = {1: 31, 2: 28, 3: 31, 4: 30, 5: 31, 6: 30, 7: 31, 8: 31, 9: 30, 10: 31, 11: 30, 12: 31}
 
 
-def first_of_month_sundays():
-    num_of_fom_sundays = 0
+def is_leap_year(year):
+    """Returns a boolean value indicating if the given year is a leap year"""
+    return year % 4 == 0 and ((not year % 100 == 0) or year % 400 == 0)
 
+
+def first_of_month_sundays():
+    """Returns the number of Sundays that fall on the first of the month from 1901-01-01 to 2000-12-31"""
+    num_of_fom_sundays = 0
+    year, month, day_of_week = 1901, 1, 2  # day_of_week: 0-6, 0 is Sunday
+    while year < 2001:
+        if day_of_week == 0:
+            num_of_fom_sundays += 1
+        day_of_week = (day_of_week + MONTH_NUM_OF_DAYS_DICT[month]) % 7
+        month += 1
+        if month == 13:
+            month = 1
+            year += 1
+    return num_of_fom_sundays
 
 
 def main():
     """Main method"""
+    print(first_of_month_sundays())
 
 
 if __name__ == "__main__":
--- problem_021.py
+++ problem_021.py
@@ -13,8 +13,43 @@
 # Evaluate the sum of all the amicable numbers under 10000.
 # Solution: ; Runtime: O()
 
+def divisor_sum(n):
+    """Returns the sum of the proper divisors of a given number n (excludes n)"""
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 0
+    elif n == 2:
+        return 1
+    else:
+        sum_of_divisors = 1
+        sqrt_n = n ** 0.5
+        for i in range(2, int(sqrt_n)+1):
+            if n % i == 0:
+                sum_of_divisors += i + int(n / i)
+        if sqrt_n.is_integer():
+            sum_of_divisors += int(sqrt_n)
+        return sum_of_divisors
+
+
+def is_amicable(n):
+    """Returns a boolean value indicating if a number is an amicable number"""
+    div_sum_n = divisor_sum(n)
+    return n == divisor_sum(div_sum_n) and n != div_sum_n
+
+
+def sum_amicable_numbers_below(n):
+    """Returns the sum of all amicable numbers below n"""
+    amicable_sum = 0
+    for i in range(n):
+        if is_amicable(i):
+            amicable_sum += i
+    return amicable_sum
+
+
 def main():
     """Main method"""
+    print(sum_amicable_numbers_below(10000))
 
 
 if __name__ == "__main__":
